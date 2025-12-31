5b03cf5d1ab44592e79e2f7f68a0f8c63619ab16
roudinebwt <roudinebwt@proton.me>
2025-12-31 08:06:45 +0100

    add a script that install all require packages

--- README.md
+++ README.md
@@ -3,16 +3,14 @@ In this dotfiles you will find my niri configuration, noctalia-shell,  fastfetch
 
 What i did to setup fedora rawhide + nix :
 
-firstly I enabled terra repo
-```
-sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-sudo dnf in terra-release-extra terra-release-mesa terra-release-multimedia
-sudo dnf update
-```
-secondly I installed nix and home manager via this *nix-installer.sh*
+firstly I enabled terra repo and installed niri
 ```
 git clone https://github.com/RoudineBWT/fedora-dotfiles.git
 cd fedora-dotfiles
+sudo bash fedora-install.sh
+```
+secondly I installed nix and home manager via this *nix-installer.sh*
+```
 sudo bash nix-install.sh
 ```
 
---
+++ fedora-install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e  # Stop on error
+repos=(
+    solopasha/hyprland
+    errornointernet/quickshell
+)
+
+for repo in "${repos[@]}"; do
+    sudo dnf5 -y copr enable $repo
+done
+
+sudo dnf install --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
+sudo dnf in terra-release-extra terra-release-mesa terra-release-multimedia
+sudo dnf update
+
+niri_packages=(
+"niri"
+"xwayland-satellite"
+"matugen"
+"xdg-desktop-portal"
+"wlsunset"
+"cava"
+"cliphist"
+"ddcutil"
+"brightnessctl"
+"swww"
+"qt6ct"
+"qt5ct"
+"nwg-look"
+"quickshell"
+"wlogout"
+"polkit-kde"
+"kf5-kirigami2"
+"kf6-kirigami"
+"qt6-qtdeclarative"
+)
+
+programming_packages=(
+  "zed"
+  "ghostty"
+)
+
+
+packages=(
+  ${niri_packages[@]}
+  ${programming_packages[@]}
+)
+
+# install rpms
+sudo dnf install ${packages[@]}
+
+# install noctalia-shell (I will try to find how to make a copr for this)
+mkdir -p ~/.config/quickshell/noctalia-shell && curl -sL https://github.com/noctalia-dev/noctalia-shell/releases/latest/download/noctalia-latest.tar.gz | tar -xz --strip-components=1 -C ~/.config/quickshell/noctalia-shell
